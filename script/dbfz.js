0c7db9820b1e412b4366f7fb4235373e274c9961
lamrouahmed <lamrouahmed@gmail.com>
2020-10-30 16:23:39 +0100

    footerSkeleton

--- script/dbfz.js
+++ script/dbfz.js
@@ -555,6 +555,7 @@ const initializeData = () => {
     charContainer.forEach((container, index) => {
         let characterColor = characterZ[charIndex(characterZ, charZ[index].dataset.char)].color;
         container.style.backgroundColor = characterColor
+        $$('.colors > div')[index].style.backgroundColor = characterColor
     })
     
 }
@@ -576,3 +577,29 @@ initializeData();
 //   .then(data => data && $('body').classList.add('loaded'))
 
 
+
+// function shadeColor(color, percent) {
+
+//     var R = parseInt(color.substring(1,3),16);
+//     var G = parseInt(color.substring(3,5),16);
+//     var B = parseInt(color.substring(5,7),16);
+
+//     R = parseInt(R * (100 + percent) / 100);
+//     G = parseInt(G * (100 + percent) / 100);
+//     B = parseInt(B * (100 + percent) / 100);
+
+//     R = (R<255)?R:255;  
+//     G = (G<255)?G:255;  
+//     B = (B<255)?B:255;  
+
+//     var RR = ((R.toString(16).length==1)?"0"+R.toString(16):R.toString(16));
+//     var GG = ((G.toString(16).length==1)?"0"+G.toString(16):G.toString(16));
+//     var BB = ((B.toString(16).length==1)?"0"+B.toString(16):B.toString(16));
+
+//     return "#"+RR+GG+BB;
+// }
+
+
+function adjust(color, amount) {
+    return '#' + color.replace(/^#/, '').replace(/../g, color => ('0'+Math.min(255, Math.max(0, parseInt(color, 16) + amount)).toString(16)).substr(-2));
+}
